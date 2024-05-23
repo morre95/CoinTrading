0e7f8b6653860ee5296e5c82d0aab3e89a2ba548
Erik Morén <erik.moren78@gmail.com>
2024-05-23 10:08:30 +0200

    Added price model

--- CoinTrading/Pages/Index.cshtml.cs
+++ CoinTrading/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace CoinTrading.Pages
 {
@@ -14,7 +16,49 @@ namespace CoinTrading.Pages
 
         public void OnGet()
         {
+            UserContext user = new UserContext();
 
+            foreach (var u in user.Users)
+            {
+                Debug.WriteLine($"{u.Username}");
+                Debug.WriteLine($"{u.Email}");
+                Debug.WriteLine($"{u.Password}");
+            }
+        }
+    }
+
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    [Keyless]
+    public class Prices
+    {
+        public string symbol { get; set; }
+        public double open_price { get; set; }
+        public double close_price { get; set; }
+        public double high_price { get; set; }
+        public double low_price { get; set; }
+        public string timestamp { get; set; }
+    }
+
+    public class UserContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Prices> Prices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Data\data.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("Users");
         }
     }
 }
---
+++ CoinTrading/Pages/Prices.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoinTrading.Pages
+{
+    public class PricesModel : PageModel
+    {
+        UserContext DB;
+        public PricesModel()
+        {
+            DB = new UserContext();
+        }
+        public JsonResult OnGet()
+        {
+            return new JsonResult(new { GustavsSuperVariabelWithPriceDataSoHeCanHaveASuperLongVariableToWorkWithBeacuseEveryProgramerLovesThat = DB.Prices.ToList() });
+        }
+    }
+}
