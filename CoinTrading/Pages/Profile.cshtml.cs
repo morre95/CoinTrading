e73f1e5f226ea0876d98a2139efd688b67ddede4
hammarhank <axtr@live.com>
2024-05-28 20:48:11 +0200

    added profile page. not done

---
+++ CoinTrading/Pages/Profile.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoinTrading.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ILogger<ProfileModel> _logger;
+
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public double Balance { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public ProfileModel(ILogger<ProfileModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Username = HttpContext.Session.GetUsername();
+            Balance = HttpContext.Session.GetBalance();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Logik för att uppdatera lösenordet
+            _logger.LogInformation("Password for user {Username} is being updated.", Username);
+
+            // Uppdatera lösenordet här
+
+            return RedirectToPage("Profile");
+        }
+        //TODO: Add a method to change the password
+        //TODO: Add a method to show user balance history
+        //TODO: Add a method to show user trade history
+    }
+}
