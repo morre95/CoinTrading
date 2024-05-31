7c26881ca8305ce6d69ed0c379dd6d64905b4ad3
Erik Morén <erik.moren78@gmail.com>
2024-05-31 13:15:53 +0200

    lagt till subscription i price.py

--- CoinTrading/price.py
+++ CoinTrading/price.py
@@ -26,7 +26,52 @@ c.execute('''CREATE TABLE IF NOT EXISTS prices
               timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 conn.commit()
 
-async def get_binance_price(symbol):
+class Manager:
+    def __init__(self):
+        self.url = "wss://stream.binance.com:9443/ws"
+        
+    async def start(self):
+        async with websockets.connect(self.url) as websocket:
+            await self.subscribe(websocket)
+            await self.listen(websocket)
+    
+    async def subscribe(self, websocket):
+        info = {"method": "SUBSCRIBE", "params": ["btcusdt@miniTicker"], "id": 1}
+        message = json.dumps(info)
+        await websocket.send(message)
+    
+    async def listen(self, websocket):
+        async for message in websocket:
+            self.handle_text_message_received(message)
+    
+    def handle_text_message_received(self, message):
+        data = json.loads(message)
+        if 'result' in data and data['result'] == None:
+            print(data)
+        else:
+            close_price = data['c']
+            open_price = data['o']
+            high_price = data['h']
+            low_price = data['l']
+            symbol = data['s']
+            print(f"{symbol.upper()} - Close: {close_price} USD, Open: {open_price} USD, High: {high_price} USD, Low: {low_price} USD")
+            # Spara priserna i databasen
+            c.execute("INSERT INTO prices (symbol, open_price, close_price, high_price, low_price) VALUES (?, ?, ?, ?, ?)", 
+                    (symbol.upper(), open_price, close_price, high_price, low_price))
+            conn.commit()
+
+            c.execute("DELETE FROM prices WHERE timestamp < DATETIME('now', '-400 seconds')")
+            conn.commit()
+
+
+def main():
+    manager = Manager()
+    asyncio.run(manager.start())
+
+if __name__ == "__main__":
+    main()
+
+""" async def get_binance_price(symbol):
     uri = f"wss://stream.binance.com:9443/ws/{symbol.lower()}@miniTicker"
 
     async with websockets.connect(uri) as websocket:
@@ -59,4 +104,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
     finally:
-        conn.close()
+        conn.close() """
