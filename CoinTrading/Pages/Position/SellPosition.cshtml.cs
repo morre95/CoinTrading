f1507fc2ea8bbf32893225300afc168ce4c3b151
Erik Morén <erik.moren78@gmail.com>
2024-06-01 11:36:50 +0200

    sell update

--- CoinTrading/Pages/Position/SellPosition.cshtml.cs
+++ CoinTrading/Pages/Position/SellPosition.cshtml.cs
@@ -1,3 +1,4 @@
+using CoinTrading.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +8,42 @@ namespace CoinTrading.Pages.Position
     {
         public IActionResult OnGet()
         {
+            if (double.TryParse(Request.Query["amount"].ToString().Replace('.', ','), out double amount) &&
+                double.TryParse(Request.Query["price"].ToString().Replace('.', ','), out double price) &&
+                int.TryParse(Request.Query["leverage"], out int leverage))
+            {
+                SystemDbContext db = new();
+
+                Users? user = db.Users.FirstOrDefault(u => u.Id == HttpContext.Session.GetUserId());
+
+                if (user != null)
+                {
+                    Positions? pos = db.Positions.FirstOrDefault(p => p.Userid == user.Id && p.is_closed == 0);
+                    if (pos != null)
+                    {
+                        double balance = HttpContext.Session.GetBalance();
+                        CoinPairs[]? coinBalance = HttpContext.Session.GetCoinBalance();
+
+                        // TODO: räkna ut och returnera något till frontend att det gick bra
+
+                        /*double btcValue = amount / price;
+                        double averageEntry = amount / btcValue;
+                        double pAndL = amount * ((1 / averageEntry) - (1 / price));
+
+                        pos.IsClosed = true;
+                        db.SaveChanges();*/
+                    }
+                    else
+                    {
+                        return new JsonResult(new { error = $"You do not have an open position" });
+                    }
+                }
+                else
+                {
+                    return new JsonResult(new { error = $"You are not logedin" });
+                }
+
+            }
             return new JsonResult(new { error = $"Sell orders is not implemented yet" });
         }
     }
